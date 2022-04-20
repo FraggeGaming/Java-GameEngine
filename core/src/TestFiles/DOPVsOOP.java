c3a8cc1d3f51ff4e2c73b4ffadf846cd35aa5a9c
dv2fnh <dv20fnh@cs.umu.se>
2022-04-20 16:10:32 +0200

    fixed som refactoring

--- core/src/TestFiles/DOPVsOOP.java
+++ core/src/TestFiles/DOPVsOOP.java
@@ -21,13 +21,7 @@ import com.badlogic.gdx.utils.ScreenUtils;
 
 public class DOPVsOOP extends ApplicationAdapter {
 	SpriteBatch batch;
-
 	Engine engine;
-	TextureAtlas atlas;
-	Entity player;
-	CollisionDetectionSystem collisionSystem;
-
-
 
 	@Override
 	public void create () {
@@ -37,75 +31,31 @@ public class DOPVsOOP extends ApplicationAdapter {
 		TDCamera camera = new TDCamera(width, height);
 		camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
 		batch = new SpriteBatch();
-		atlas = new TextureAtlas("atlas/TexturePack.atlas");
-		TextureAtlas fireAtlas = new TextureAtlas("atlas/Fire.atlas");
+
 		//engine setup
 		engine = new Engine(batch, camera);
 
 		engine.addSystem(new UI());
 		engine.addSystem(new MovementSystem());
-		engine.addSystem(new WorldSystem(atlas));
-		collisionSystem = (CollisionDetectionSystem) engine.getSystem(CollisionDetectionSystem.class);
-
-
-		createPlayers(camera);
-
-		for (int i = 0; i < 5; i++){
-			for (int j = 0; j < 5; j++)
-			createFireAnimationTest(30 * i, 30*j, fireAtlas );
-		}
-
+		engine.addSystem(new WorldSystem());
 
 		//to stuff with netWork
 		NetWorkClient client = new NetWorkClient();
 		engine.addNetWorkClientOnUpdate(client);
 
+		//TODO sorting system for systems based on priority order
+		//TODO collision filter
+		//TODO add UpNp
+		//TODO UDP support
+		//TODO create mouse clicking system
+		//TODO raycast component
+		//TODO audio component
+		//TODO Physics system
+		//TODO PP
+		//TODO some kind of particle system (gpu calculated?)
+		//TODO navMesh (multithreaded pathfinding)
 
 	}
-
-	public void createFireAnimationTest(int x, int y, TextureAtlas fireAtlas){
-		Entity e = new Entity();
-		e.addComponents(new TextureComponent(new TextureRegion(fireAtlas.findRegion("fire", 0))));
-		e.addComponents(new TransformComponent(x, y, 1, 120, 120));
-		CollisionComponent c = new CollisionComponent(x + 20, y + 20, 40, 40);
-		c.id = "Fire";
-		c.isStatic = true;
-		e.addComponents(c);
-		Animation animation = new Animation(fireAtlas, "fire", 109);
-		AnimationComponent a = new AnimationComponent(0.02f, true, animation.getFrames());
-		a.addAnimation(0.3f, true, animation.getFrames());
-		e.addComponents(a);
-
-		engine.addEntity(e);
-	}
-
-
-
-	public void createPlayers(TDCamera camera){
-		engine.user = "Player"; //TODO change this later to less shit way
-
-		player = new Entity();
-		player.addComponents(new TextureComponent(new TextureRegion(atlas.findRegion("RedAnt"))));
-		player.addComponents(new TransformComponent(camera.viewportWidth / 2, camera.viewportHeight / 2, 5, 30, 30));
-		CollisionComponent c = new CollisionComponent(100, 100, 30, 30);
-		c.id = "Player2";
-		player.addComponents(c);
-		player.addComponents(new VelocityComponent());
-		player.tag = "Player2";
-		engine.addEntity(player);
-
-		player = new Entity();
-		player.addComponents(new TextureComponent(new TextureRegion(atlas.findRegion("BlueAnt"))));
-		player.addComponents(new TransformComponent(camera.viewportWidth / 2, camera.viewportHeight / 2, 5, 30, 30));
-		c = new CollisionComponent(100, 100, 30, 30);
-		c.id = "Player";
-		player.addComponents(c);
-		player.addComponents(new VelocityComponent());
-		player.tag = "Player";
-		engine.addEntity(player);
-	}
-
-
 
 
 	@Override
@@ -113,14 +63,10 @@ public class DOPVsOOP extends ApplicationAdapter {
 		engine.update(Gdx.graphics.getDeltaTime());
 	}
 
-
-
 	@Override
 	public void dispose () {
 		batch.dispose();
 	}
-
-
 
 
 }
--- core/src/TestFiles/scripts/WorldSystem.java
+++ core/src/TestFiles/scripts/WorldSystem.java
@@ -1,9 +1,14 @@
 package TestFiles.scripts;
 
+import EntityEngine.Components.*;
+import EntityEngine.Entity;
+import EntityEngine.GameClasses.Animation;
+import EntityEngine.GameClasses.TDCamera;
 import EntityEngine.Noise.OpenSimplexNoise;
 import EntityEngine.Systems.System;
 import EntityEngine.Tile;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class WorldSystem extends System {
     int tileMapRenderIndexX = 0;
@@ -14,11 +19,62 @@ public class WorldSystem extends System {
     int mapSizeIndex = 0;
     OpenSimplexNoise noise;
     TextureAtlas atlas;
-    public WorldSystem(TextureAtlas atlas){
+
+    Entity player;
+    public WorldSystem(){
         noise = new OpenSimplexNoise(); //for tilemap generation
-        this.atlas = atlas;
+        atlas = new TextureAtlas("atlas/TexturePack.atlas");
+
+        TextureAtlas fireAtlas = new TextureAtlas("atlas/Fire.atlas");
+        for (int i = 0; i < 5; i++){
+            for (int j = 0; j < 5; j++)
+                createFireAnimationTest(30 * i, 30*j, fireAtlas );
+        }
+
+        createPlayers(engine.camera);
+
+
     }
 
+    public void createPlayers(TDCamera camera){
+        engine.user = "Player"; //TODO change this later to less shit way
+
+        player = new Entity();
+        player.addComponents(new TextureComponent(new TextureRegion(atlas.findRegion("RedAnt"))));
+        player.addComponents(new TransformComponent(camera.viewportWidth / 2, camera.viewportHeight / 2, 5, 30, 30));
+        CollisionComponent c = new CollisionComponent(100, 100, 30, 30);
+        c.id = "Player2";
+        player.addComponents(c);
+        player.addComponents(new VelocityComponent());
+        player.tag = "Player2";
+        engine.addEntity(player);
+
+        player = new Entity();
+        player.addComponents(new TextureComponent(new TextureRegion(atlas.findRegion("BlueAnt"))));
+        player.addComponents(new TransformComponent(camera.viewportWidth / 2, camera.viewportHeight / 2, 5, 30, 30));
+        c = new CollisionComponent(100, 100, 30, 30);
+        c.id = "Player";
+        player.addComponents(c);
+        player.addComponents(new VelocityComponent());
+        player.tag = "Player";
+        engine.addEntity(player);
+    }
+
+    public void createFireAnimationTest(int x, int y, TextureAtlas fireAtlas){
+        Entity e = new Entity();
+        e.addComponents(new TextureComponent(new TextureRegion(fireAtlas.findRegion("fire", 0))));
+        e.addComponents(new TransformComponent(x, y, 1, 120, 120));
+        CollisionComponent c = new CollisionComponent(x + 20, y + 20, 40, 40);
+        c.id = "Fire";
+        c.isStatic = true;
+        e.addComponents(c);
+        Animation animation = new Animation(fireAtlas, "fire", 109);
+        AnimationComponent a = new AnimationComponent(0.02f, true, animation.getFrames());
+        a.addAnimation(0.3f, true, animation.getFrames());
+        e.addComponents(a);
+
+        engine.addEntity(e);
+    }
 
     @Override
     public void update(float dt) {
