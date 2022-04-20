256c48f8dbab17c6a1543d612ec969f0cb4f1535
dv2fnh <dv20fnh@cs.umu.se>
2022-04-20 15:29:07 +0200

    added my testing files

---
+++ core/src/TestFiles/scripts/Data.java
@@ -0,0 +1,35 @@
+package TestFiles.scripts;
+
+import EntityEngine.Components.Component;
+import EntityEngine.Components.TransformComponent;
+import EntityEngine.Components.VelocityComponent;
+import EntityEngine.Entity;
+import EntityEngine.Network.NetWorkData;
+import com.badlogic.gdx.utils.Array;
+
+public class Data extends NetWorkData {
+    public TransformComponent component;
+    public int entityID;
+    public String tag;
+    public Data(){
+
+    }
+
+    public void addComponent(TransformComponent component){
+       this.component = component;
+    }
+
+    public void setTag(String tag){
+        this.tag = tag;
+    }
+
+    public void setId(int i){
+        entityID = i;
+    }
+
+    public void addEntity(Entity e){
+
+    }
+
+
+}
